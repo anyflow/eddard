47576453349ddc07c7a534ce0fc6e0e1eff76868
Park Hyunjeong <anyflow@gmail.com>
2016-03-06 20:57:25 +0900

    react web resources and index.htm added.

--- src/main/java/com/lge/stark/smp/smpframe/OpCode.java
+++ src/main/java/com/lge/stark/smp/smpframe/OpCode.java
@@ -14,54 +14,44 @@ public enum OpCode {
 	INITIALIZE(0, "Session initialization"),
 
 	// 50 ~ 99 server to client code
-	SEND_NOTIFICATION_MESSAGE(50, "Sends a notification message"), SET_HEARTBEAT_RATE(51,
-			"Set heartbeat rate"), REDIRECT(52, "Redirect"),
+	SEND_NOTIFICATION_MESSAGE(50, "Sends a notification message"),
+	SET_HEARTBEAT_RATE(51, "Set heartbeat rate"),
+	REDIRECT(52, "Redirect"),
 
 	// 100 ~ 149 common code
-			CLOSE_SESSION(100, "Inform the sender will close the session"), IS_ALIVE(101,
-					"Check whether the opponent is alive and connected to its session"),
+	CLOSE_SESSION(100, "Inform the sender will close the session"),
+	IS_ALIVE(101, "Check whether the opponent is alive and connected to its session"),
 
 	// 150 ~ 199 error code
-					ERROR_INTERNAL_UNKNOWN(150,
-							"Error occurred internally with unknown reason"), ERROR_INVALID_SMPFRAME_FORMAT(151,
-									"Returns invalid smpframe format error"), ERROR_NO_SESSION_ALLOCATED(153,
-											"No session has been allocated"), ERROR_SESSION_EXPIRED(154,
-													"The session({}) expired"), ERROR_INVALID_DEVICEID(155,
-															"Device ID({}) validation failed"), ERROR_INVALID_NETWORKTYPE(
-																	157,
-																	"Inputed network type({}) is invalid"), ERROR_STARK_SERVICE(
-																			160,
-																			"Error of Stark Service. See sub error message"),
+	ERROR_INTERNAL_UNKNOWN(150, "Error occurred internally with unknown reason"),
+	ERROR_INVALID_SMPFRAME_FORMAT(151, "Returns invalid smpframe format error"),
+	ERROR_NO_SESSION_ALLOCATED(153, "No session has been allocated"),
+	ERROR_SESSION_EXPIRED(154, "The session({}) expired"),
+	ERROR_INVALID_DEVICEID(155, "Device ID({}) validation failed"),
+	ERROR_INVALID_NETWORKTYPE(157, "Inputed network type({}) is invalid"),
+	ERROR_STARK_SERVICE(160, "Error of Stark Service. See sub error message"),
 
 	// 200 ~ 255 return code
-																			RETURN_OK(200, "Processed Successfully"),
+	RETURN_OK(200, "Processed Successfully"),
 
 	// 300 ~ Eddard specific
-																			REGISTER_DEVICE(300,
-																					"Register a device"), DELETE_DEVICE(
-																							301,
-																							"Delete a device"), UPDATE_DEVICE_STATUS(
-																									302,
-																									"Update status of a device"), RETRIEVE_FRIENDS(
-																											303,
-																											"Get friends"), CREATE_CHANNEL(
-																													304,
-																													"Create a channel"), CREATE_MESSAGE(
-																															305,
-																															"Create a message"), LEAVE_CHANNEL(
-																																	306,
-																																	"Leave a channel"), MESSAGE_RECEIVED(
-																																			307,
-																																			"A message received"), GET_USERS(
-																																					308,
-																																					"Get users"), RETRIEVE_MESSAGES(
-																																							309,
-																																							"Retrieve messages of the specific channel"),
+	REGISTER_DEVICE(300, "Register a device"),
+	DELETE_DEVICE(301, "Delete a device"),
+	UPDATE_DEVICE_STATUS(302, "Update status of a device"),
+	RETRIEVE_FRIENDS(303, "Get friends"),
+	CREATE_CHANNEL(304, "Create a channel"),
+	CREATE_MESSAGE(305, "Create a message"),
+	LEAVE_CHANNEL(306, "Leave a channel"),
+	MESSAGE_RECEIVED(307, "A message received"),
+	GET_USERS(308, "Get users"),
+	RETRIEVE_MESSAGES(309, "Retrieve messages of the specific channel"),
 
-	USERS_RETRIEVED(353, "Return of GET_FRIEND / GET_USERS"), CHANNEL_CREATED(354,
-			"Return of CREATE_CHANNEL"), MESSAGE_CREATED(355, "Return of CREATE_MESSAGE"), CHANNEL_LEFT(356,
-					"A user left the channel"), UNREAD_COUNT_CHANGED(357,
-							"Unread count changed"), MESSAGES_RETRIEVED(358, "Return of RETRIEVE_MESSAGES"),
+	USERS_RETRIEVED(353, "Return of GET_FRIEND / GET_USERS"),
+	CHANNEL_CREATED(354, "Return of CREATE_CHANNEL"),
+	MESSAGE_CREATED(355, "Return of CREATE_MESSAGE"),
+	CHANNEL_LEFT(356, "A user left the channel"),
+	UNREAD_COUNT_CHANGED(357, "Unread count changed"),
+	MESSAGES_RETRIEVED(358, "Return of RETRIEVE_MESSAGES"),
 
 	INVALID_OPCODE(999, "The Invalid OpCode");
 
---
+++ src/main/resources/META-INF/web/js/index.js
@@ -0,0 +1,10 @@
+var Hello = React.createClass({
+  render: function() {
+    return (<h1>Hello! {this.props.name}</h1>);
+  }
+});
+
+ReactDOM.render(
+  <Hello name="This is Stark Project Home!" />,
+  document.getElementById('container')
+);
